e8e29fab4b007fae73fdc1e8dcd3efaa4173d27f
visualserg <visualserg@gmail.com>
2018-09-14 09:50:46 +0300

    remove sudo

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ echo "--> Updating CentOS System"
 yum -y update
 
 echo "--> Install docker"
-sudo curl -sS https://get.docker.com/ | sh
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo systemctl status docker
+curl -sS https://get.docker.com/ | sh
+systemctl start docker
+systemctl enable docker
+systemctl status docker
