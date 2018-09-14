f6a514ae79a646c07e8dd7f13818cfc8b2583a22
visualserg <visualserg@gmail.com>
2018-09-14 09:53:16 +0300

    add portainer

--- script.sh
+++ script.sh
@@ -7,3 +7,7 @@ curl -sS https://get.docker.com/ | sh
 systemctl start docker
 systemctl enable docker
 systemctl status docker
+
+echo "--> Install portainer"
+docker volume create portainer_data
+docker run --restart=always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
