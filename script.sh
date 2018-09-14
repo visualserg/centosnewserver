bc957b127a9c46e4faf6822c4163a7be5ba94b8a
visualserg <visualserg@gmail.com>
2018-09-14 09:49:37 +0300

    add docker

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 echo "--> Updating CentOS System"
 yum -y update
+
+echo "--> Install docker"
+sudo curl -sS https://get.docker.com/ | sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
